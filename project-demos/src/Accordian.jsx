6dfa2ca8d1cc72e6f3246db02d597e98add2b40f
meow-codes201 <humairat291@gmail.com>
2026-06-21 21:09:10 +0530

    accordion

---
+++ project-demos/src/Accordian.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import styles from "./Accordian.module.css";
+function Accordian({ id, title, content }) {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <>
+      <div className={styles.bdy} key={id}>
+        <div className={styles.ctr}>
+          <div className={styles.hdr} onClick={() => setIsActive(!isActive)}>
+            <div className={styles.tl}>{title}</div>
+            <span className={styles.icon}>{isActive ? "-" : "+"}</span>
+          </div>
+
+          <div>
+            {isActive && <p className={styles.cardContent}>{content}</p>}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Accordian;
